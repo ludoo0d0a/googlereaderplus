bec49198782b99a681e6802b0dddd7c0120ea59d
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2010-01-28 10:10:59 +0000

    +portal theme

--- GoogleReaderPlus/src/skin/portal.js
+++ GoogleReaderPlus/src/skin/portal.js
@@ -1,7 +1,8 @@
 //Portal theme
 
 GRP.portal = function() {
-	var css = '.entry{float:left;max-width:400px;padding:2px!important;}.entry .entry-title {font-size:100%!important;}.entry .card-bottom{display:none;}';
+	var css = '.entry{float:left;max-width:400px;padding:2px!important;}.entry .entry-title {font-size:100%!important;}.entry .card-bottom{display:none;}#scroll-filler{float:left;}';
 	GM_addStyle(css);
+	
 	fireResize();
 };
