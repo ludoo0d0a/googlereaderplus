7a4884cbf576d908f51e3872e07f3c27072ee3fd
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2010-02-05 11:18:14 +0000

    2.8.5 + button 1.0.1

--- GoogleReaderPlus/src/version.js
+++ GoogleReaderPlus/src/version.js
@@ -2,7 +2,7 @@
  * @author Valente
  */
 window.GRP={
-	VERSION: "2.8.4",
+	VERSION: "2.8.5",
 	setVersion:function(text){
 		var ver = document.getElementById('version');
 		ver.innerHTML = (text||'') + GRP.VERSION;
