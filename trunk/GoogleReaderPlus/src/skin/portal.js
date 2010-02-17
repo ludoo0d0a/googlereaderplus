45d8a0d6464b81d2e57a0206f513a12f228afa20
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2010-02-17 07:49:20 +0000

    portal in 2 columns

--- trunk/GoogleReaderPlus/src/skin/portal.js
+++ trunk/GoogleReaderPlus/src/skin/portal.js
@@ -1,7 +1,8 @@
 //Portal theme
 
 GRP.portal = function() {
-	var css = '.entry{float:left;max-width:400px;padding:2px!important;}.entry .entry-title {font-size:100%!important;}.entry .card-bottom{display:none;}#scroll-filler{float:left;}';
+	var w = Math.round((getWidthEntries()-50)/2);
+	var css = '.entry{float:left;width:'+w+'px;padding:2px!important;}.card-common{margin:0;}.entry .entry-title {font-size:100%!important;}.entry .card-bottom,.entry-author,.entry-date{display:none;}#scroll-filler{float:left;}';
 	GM_addStyle(css);
 	
 	//fireResize();
