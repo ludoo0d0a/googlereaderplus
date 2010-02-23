971cf0aa1e9d93322e815700a7d569092e2d59fd
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2010-02-23 15:53:23 +0000

    button update name + lang + logo

--- GoogleReaderPlus/button/version.js
+++ GoogleReaderPlus/button/version.js
@@ -2,7 +2,7 @@
  * @author Valente
  */
 window.GRPP={
-	VERSION: "1.0.1",
+	VERSION: "1.1.0",
 	setVersion:function(text){
 		var ver = document.getElementById('version');
 		ver.innerHTML = (text||'') + GRP.VERSION;
