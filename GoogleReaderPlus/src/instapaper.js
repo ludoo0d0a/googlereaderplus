44e3987e9152dbe56699b86a9de2d3a6fc01542c
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2010-02-24 13:41:55 +0000

    nologin alert

--- GoogleReaderPlus/src/instapaper.js
+++ GoogleReaderPlus/src/instapaper.js
@@ -118,6 +118,7 @@ GRP.instapaper = function(prefs, langs){
         */
         if (typeof username === "undefined" || username === '') {
             alert(SL.nologin);
+			return false;
 			/*username = prompt(SL.login);
             username = username || '';
 			if (username===''){
@@ -136,8 +137,8 @@ GRP.instapaper = function(prefs, langs){
         };
     }
     function clearAuth(){
-        GM_setValue('instapaper_username', '');
-        GM_setValue('instapaper_password', '');
+        //GM_setValue('instapaper_username', '');
+        //GM_setValue('instapaper_password', '');
     }
     
     //var css = ".instapaper{margin-left: 4px;padding-left:16px;width:14px;height:14px;background-repeat:no-repeat;background-image:url(data:image/png,%89PNG%0D%0A%1A%0A%00%00%00%0DIHDR%00%00%00%10%00%00%00%10%08%06%00%00%00%1F%F3%FFa%00%00%00%01sRGB%00%AE%CE%1C%E9%00%00%00%04gAMA%00%00%B1%8F%0B%FCa%05%00%00%00%20cHRM%00%00z%26%00%00%80%84%00%00%FA%00%00%00%80%E8%00%00u0%00%00%EA%60%00%00%3A%98%00%00%17p%9C%BAQ%3C%00%00%00%09pHYs%00%00%0E%C3%00%00%0E%C3%01%C7o%A8d%00%00%00%18tEXtSoftware%00Paint.NET%20v3.22%B7%91%17%7D%00%00%00%84IDAT8Oc%60%18%B4%E0%DD%9Bw%FFa%18%E4Hd6AG%3F%7B%F2%EC%FF%CE%DD%3B%E1%F8%C0%BE%03%FF%9F%3D%B9%0F6%90%A0f%90%82%D7%2F%9F%FD_%B4h%D1%FF%EE%DE%EE%FF%93%A7N%06%B3%AF_%BDN%BC%01%20C%40%AE%00%19%00%C2%20%CDD%D9%8C%AC%08%DD%00%90%ABH2%04f%00%C8%0B%24%3B%1F%D9%0B%14%1B%00%0B%03%B2%BD%40V%20R%1C%8D%14%27%24%E4%A4%0BK%7D%24%25e%92%E2%9BR%C5%00%82%C7%DB%C3L%A1%1D%CB%00%00%00%00IEND%AEB%60%82);}";
