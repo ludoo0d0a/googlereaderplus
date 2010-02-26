046a92378e16b9c6dcc2dc577960dddad88372e1
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2010-02-26 10:51:00 +0000

    general with icon +  https

---
+++ trunk/GoogleReaderPlus/src/general.js
@@ -0,0 +1,14 @@
+/**
+ * General configuration
+ * @version  0.1
+ * @date 2010
+ *
+ */
+GRP.general = function(prefs, langs){
+
+	if (prefs.secure){
+		if (/^http\:/.test(window.location.href)) {
+			window.location.href=window.location.href.replace(/^http\:/, 'https:');
+		}
+	}
+};
