90e2ce1e7fdfcef3a7e70c7272c67acf38a0df2e
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2010-02-26 10:49:47 +0000

    shortcut bugfix

--- GoogleReaderPlus/src/instapaper.js
+++ GoogleReaderPlus/src/instapaper.js
@@ -21,7 +21,7 @@ GRP.instapaper = function(prefs, langs){
     
     function addButton(el, entry, mode){
         var text = SL.text + formatShortcut('instapaper', 'readitlater', prefs); //[b]
-        addBottomLink(el, SL.keyword, text, 'instapaper item-star star', false, instapaperShare, false, entry, mode);
+        addBottomLink(el, SL.keyword, text, 'btn-instapaper item-star star', false, instapaperShare, false, entry, mode);
     }
     
     function addKey(){
