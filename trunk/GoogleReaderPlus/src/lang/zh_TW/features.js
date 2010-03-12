26a92e8d4167d4f45fd9cda95e2ed0873dce99cf
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2010-03-12 08:44:47 +0000

    +zh_TW

---
+++ trunk/GoogleReaderPlus/src/lang/zh_TW/features.js
@@ -0,0 +1,223 @@
+/**
+ * ReaderPlus
+ * Translation for features
+ * 
+ * **************************
+ * 翻譯 : 繁體中文 By shell9000
+ * **************************
+ * 
+ * Version : 0.1.1
+ * Date : 2010-03-04
+ * @author shell9000
+ */
+namespace('GRP.langs.zh_TW');
+GRP.langs.zh_TW.scripts = [
+{
+    id: "general",
+    name: "General",
+    desc: "General configuration"
+},
+{
+    id: "theme",
+    name: "主題",
+    desc: "更改Google Reader界面皮膚"
+}, 
+/*
+{
+    id: "language_lang",
+    name: "語言",
+    desc: "更改這個擴展的語言"
+ 
+}, 
+*/
+{
+    id: "favicons",
+    name: "Favicons",
+    desc: "顯示每個Feed的Favicon(網站小圖標)"
+}, 
+{
+    id: "unreadcount",
+    name: "顯示所有未讀計數",
+    desc: "顯示所有未讀計數的Feeds"
+}, 
+{
+    id: "fixlayout",
+    name: "固定佈局",
+    desc: "修正錯誤一樣不同的佈局入門全寬式，修復丟失的外殼圖片，使大圖像適合屏幕"
+}, 
+{
+    id: "count",
+    name: "修正計數(1000+)",
+    desc: "實時更新顯示準確的未讀計數"
+}, 
+{
+    id: "counticon",
+    name: "圖標計數器",
+    desc: "在Google Reader Favicon上顯示準確的計數"
+}, 
+{
+    id: "removeads",
+    name: "刪除廣告",
+    desc: "簡單的廣告攔截器"
+}, 
+{
+    id: "column",
+    name: "多列文本",
+    desc: "添加一個按鈕，顯示為多列閱讀"
+}, 
+{
+    id: "preview",
+    name: "集成預覽",
+    desc: "添加一個按鈕，在Google Reader內部顯示整個閱讀頁面，而不需要打開新標籤或新窗口"
+}, 
+{
+    id: "colorful",
+    name: "彩色列表",
+    desc: "使用同一Feed背景色"
+}, 
+{
+    id: "filter",
+    name: "過濾器",
+    desc: "通過消除或高亮用戶的條件為基礎的項目的篩選器項目"
+}, 
+{
+    id: "readbymouse",
+    name: "鼠標閱讀",
+    desc: "窗口中鍵單擊不可用",
+    desc: "下一個/上一個(Feed) 使用鼠標左/右鍵單擊"
+}, 
+{
+    id: "facebook",
+    name: "發送到Facebook",
+    desc: "添加一個按鈕來分享信息到Facebook"
+}, 
+{
+    id: "twitter",
+    name: "發送到Twitter",
+    desc: "添加一個按鈕來分享信息到Twitter"
+}, 
+{
+    id: "instapaper",
+    name: "發送到Instapaper",
+    desc: "添加一個按鈕,發送到Instapaper.<br>【中文解釋:留著以後閱讀】詳情可以參考官網 http://goo.gl/yip4"
+}, 
+{
+    id: "mark",
+    name: "標記已讀",
+    desc: "快捷鍵控制當前閱讀的前/後條目為已讀狀態"
+}, 
+{
+    id: "jump",
+    name: "添加頂部/底部鏈接",
+    desc: "添加返回頂部和跳轉底部的功能按鈕(適合閱讀篇幅比較長的Feed)"
+}, 
+{
+    id: "fitheight",
+    name: "固定高度",
+    desc: "相對於屏幕高度的當前新聞的適合高度(長文章)"
+}, 
+{
+    id: "closeentry",
+    name: "關閉條目",
+    desc: "給每個條目添加一個'關閉'按鈕，將其刪除"
+}, 
+{
+    id: "openbackground",
+    name: "後台閱讀",
+    desc: "給每個條目添加一個'後台打開'按鈕"
+}, 
+{
+    id: "replacer",
+    name: "替換腳本",
+    desc: "替換進入原始網頁的一部分"
+},
+/*
+{
+    id: "menu",
+    name: "Intuitive menu",
+    desc: "Intuitive menu to add extra capabilites"
+}, */
+{
+    id: "aero",
+    name: "Google Aero 工具欄",
+    desc: "工具欄使用Aero主題"
+}, 
+{
+    id: "info",
+    name: "系統信息",
+    desc: "預覽當前的系統綜合信息或屬性"
+}, 
+{
+    id: "extshortcuts",
+    name: "快捷鍵",
+    desc: "鍵盤快捷鍵"
+}, 
+{
+    id: "pack",
+    name: "快速設置",
+    desc: "預先設置的的配置參數"
+
+}, 
+{
+    id: "thanks",
+    name: "鳴謝",
+    desc: ""
+
+}, 
+{
+    id: "about",
+    name: "關於",
+    desc: "關於 Reader Plus"
+
+}];
+
+
+GRP.langs.zh_TW.skins = [
+{
+    id: "none",
+    name: "None"
+}, 
+{
+    id: "air",
+    name: "Air Skin"
+}, 
+{
+    id: "aircomic",
+    name: "Air Skin Comic Sans"
+}, 
+{
+    id: "black",
+    name: "Google Enhanced Black"
+}, 
+{
+    id: "dark",
+    name: "Dark Skin"
+}, 
+{
+    id: "darkgray",
+    name: "Dark Gray Skin"
+}, 
+{
+    id: "helvetireader",
+    name: "Helvetireader Skin"
+}, 
+{
+    id: "minimal",
+    name: "Minimalistic Skin"
+}, 
+{
+    id: "optimized",
+    name: "Optimized Skin"
+}, 
+{
+    id: "portal",
+    name: "Portal Theme"
+}, 
+{
+    id: "osxblue",
+    name: "Mac OS X Snow Leopard - Blue"
+}, 
+{
+    id: 'osxblack',
+    name: "Mac OS X Snow Leopard - Black"
+}];
