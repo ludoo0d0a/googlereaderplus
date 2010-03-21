f3ba6e47f088bbfcbc3112d1528bed5cc587048b
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2010-03-21 18:04:58 +0000

    russian

---
+++ GoogleReaderPlus/src/lang/ru/features.js
@@ -0,0 +1,232 @@
+﻿/**
+ * ReaderPlus
+ * Translation for features
+ * 
+ * **************************
+ * ru : Russian
+ * **************************
+ * 
+ * For translators : please keep major version or original for your translations !
+ * Use minor version for yours translations 
+ * so that 0.3.11 is the 11th translated version of the original v0.3 english version.  
+ * 
+ * 
+ * Version : 0.4.1
+ * Date : 03-21-2010
+ * @author Ruslan Pokatskii
+ */
+namespace('GRP.langs.ru');
+GRP.langs.ru.scripts = [
+{
+    id: "general",
+    name: "Главная",
+    desc: "Основные настройки"
+},
+{
+    id: "icon",
+    name: "Кнопка",
+    desc: "Добавить кнопку на панель Google Chrome"
+},
+{
+    id: "theme",
+    name: "Темы",
+    desc: "Изменение внешнего вида Google Reader"
+}, 
+{
+    id: "favicons",
+    name: "Иконки подписок",
+    desc: "Отображение собственных иконок для каждой подписки"
+}, 
+{
+    id: "unreadcount",
+    name: "Счётчик непрочитанных",
+    desc: "Отображение числа непрочитанных сообщений для каждой подписки"
+}, 
+{
+    id: "fixlayout",
+    name: "Поправка макета",
+    desc: "Исправление различных недостатков макета стиля - заполнение во всю ширину, отображение картинок, масштабирование больших изображений по размеру окна и т.п."
+}, 
+{
+    id: "count",
+    name: "Поправка счётчика (1000+)",
+    desc: "Исправление неправильного отображения числа непрочитанных записей при количестве более 1000"
+}, 
+{
+    id: "counticon",
+    name: "Счётчик на вкладке",
+    desc: "Отображение количества непрочитанных в заголовке вкладки"
+}, 
+{
+    id: "removeads",
+    name: "Удаление рекламы",
+    desc: "Простой блокировщик рекламы"
+}, 
+{
+    id: "column",
+    name: "Текст колонками",
+    desc: "Добавление кнопки отображения сообщений колонками, как в газете"
+}, 
+{
+    id: "preview",
+    name: "Просмотр оригинала",
+    desc: "Добавление кнопки отображения оригинальной страницы вместо короткой записи"
+}, 
+{
+    id: "colorful",
+    name: "Разноцветный список",
+    desc: "Выделение соответствующим цветным фоном записей разных подписок"
+}, 
+{
+    id: "filter",
+    name: "Фильтр записей",
+    desc: "Удаление или выделение записей по шаблону пользователя"
+}, 
+{
+    id: "readbymouse",
+    name: "Чтение мышью",
+    desc: "Средняя кнопка не доступна в Windows",
+    desc: "Переход к следующему/предидущему сообщению используя правую/левую кнопки мыши"
+}, 
+{
+    id: "facebook",
+    name: "Интеграция с Facebook",
+    desc: "Кнопка публикации новостей в Facebook"
+}, 
+{
+    id: "twitter",
+    name: "Интеграция с Twitter",
+    desc: "Кнопка отправки новостей в Twitter"
+}, 
+{
+    id: "instapaper",
+    name: "Интеграция с Instapaper",
+    desc: "Кнопка добавления сообщений в Instapaper для чтения позже"
+}, 
+{
+    id: "mark",
+    name: "Пометить как прочитанное",
+    desc: "Пометка всех записей прочитанными перед/после текущей записи"
+}, 
+{
+    id: "jump",
+    name: "Кнопки начало/конец",
+    desc: "Кнопки 'Перейти к началу записи' и 'Перейти к концу записи'"
+}, 
+{
+    id: "fitheight",
+    name: "Во всю длинну",
+    desc: "Кнопка растягивания сообщения во всю длинну (для длинных сообщений)"
+}, 
+{
+    id: "closeentry",
+    name: "Закрытие сообщения",
+    desc: "Кнопка 'закрыть' для каждого сообщения"
+}, 
+{
+    id: "openbackground",
+    name: "Фоновое открытие",
+    desc: "Кнопка 'открыть в фоне' в сообщениях"
+}, 
+{
+    id: "replacer",
+    name: "Заменитеь сообщений",
+    desc: "Замена части оригинального сообщения на комиксы с gocomics.com. <br/>Спасибо jolan78 за его оригинальную идею и скрипт."
+},
+/*
+{
+    id: "menu",
+    name: "Интуитивное меню",
+    desc: "Интуитивное меню для добавления особых возможностей"
+}, */
+{
+    id: "lightbox",
+    name: "Lightbox",
+	desc: "Просмотр медиа на весь экран"
+}, 
+{
+    id: "relook",
+    name: "Изменение вида",
+    desc: "Изменение внешнего вида с использованием пользовательских стилей"
+}, 
+{
+    id: "aero",
+    name: "Панель Google Aero",
+    desc: "Использование панели из темы Aero"
+}, 
+{
+    id: "info",
+    name: "О системе",
+    desc: "Системная Информация"
+}, 
+{
+    id: "extshortcuts",
+    name: "Сочетания клавиш",
+    desc: "Сочетания клавиш клавиатуры"
+}, 
+{
+    id: "pack",
+    name: "Конфигурации",
+    desc: "Наборы настроек"
+
+}, 
+{
+    id: "thanks",
+    name: "Благодарности",
+    desc: ""
+}, 
+{
+    id: "about",
+    name: "Информация",
+    desc: "Информация о ReaderPlus"
+}];
+
+GRP.langs.ru.skins = [
+{
+    id: "none",
+    name: "Нет"
+}, 
+{
+    id: "air",
+    name: "Air Skin"
+}, 
+{
+    id: "aircomic",
+    name: "Air Skin Comic Sans"
+}, 
+{
+    id: "black",
+    name: "Google Enhanced Black"
+}, 
+{
+    id: "dark",
+    name: "Dark Skin"
+}, 
+{
+    id: "darkgray",
+    name: "Dark Gray Skin"
+}, 
+{
+    id: "helvetireader",
+    name: "Helvetireader Skin"
+}, 
+{
+    id: "minimal",
+    name: "Minimalistic Skin"
+}, 
+{
+    id: "optimized",
+    name: "Optimized Skin"
+}, 
+{
+    id: "portal",
+    name: "Portal Theme"
+}, 
+{
+    id: "osxblue",
+    name: "Mac OS X Snow Leopard - Blue"
+}, 
+{
+    id: 'osxblack',
+    name: "Mac OS X Snow Leopard - Black"
+}];
