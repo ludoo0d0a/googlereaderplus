3bd6c66808147d26fd0794d48b7bbfe527b28754
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2010-03-29 08:14:20 +0000

    replacer new config

--- GoogleReaderPlus/src/replacer.js
+++ GoogleReaderPlus/src/replacer.js
@@ -1,41 +1,36 @@
 /**
  * TODO make doc
- * @version  0.1
+ * @version  0.2
  * @date 2010
  * @author JoLan
  * Replace entries with part of the original article
  *
  */
 GRP.replacer = function(prefs, langs){
-    var locked = false, SL = langs.replacer, partIndex = 0, TPL_NAME = "replacer_result_";
-    var gp_links, gp_from, gp_to, re_from = [], re_links = [];
+    var locked = false, SL = langs.replacer, partIndex = 0, gp_data={}, TPL_NAME = "replacer_result_";
     
     function initVars(){
-        gp_links = prefs.replacer_link.split("\n");
-        gp_from = prefs.replacer_from.split("\n");
-        gp_to = prefs.replacer_to.split("\n");
-        
-        for (var i = 0; i < gp_from.length; i++) {
-            if (gp_from[i] && gp_links[i]) {
-                re_from[i] = new RegExp(gp_from[i], "i");
-                re_links[i] = new RegExp(gp_links[i], "i");
-            }
-        }
+		gp_data={};
+		iterate(prefs.replacer_items, function(title,o){
+			  o.re_url=new RegExp(o.url, "i");
+			  o.re_search=new RegExp(o.search, "i");
+			  gp_data[title]=o;
+		});
     }
     
-    function replaceItem(html, indexes, partIndex){
-        var result = "";
-        for (var i = 0, len = indexes.length; i < len; i++) {
-            var index = indexes[i];
-            var m = re_from[index].exec(html);
-            if (m && (m.length > 0)) {
-                var matchingtext = m[0];
-                result += matchingtext.replace(re_from[index], gp_to[index]);
+    function replaceItem(html, matches, partIndex){
+        var result = '';
+       
+	   iterate(matches, function(title, o){
+            var item = gp_data[title];
+			var m = item.re_search.exec(html);
+            if (m && m[0]) {
+                result += m[0].replace(item.re_search, item.replace);
             }
-            
-        }
+        });
+		
         var el = document.getElementById(TPL_NAME + partIndex);
-        if (result !== "") {
+        if (result !== '') {
             el.innerHTML = result;
         } else {
             el.innerHTML = SL.nomatch;
@@ -46,29 +41,23 @@ GRP.replacer = function(prefs, langs){
 				entryBody.style.display='';
 			}
         }
-        
     }
     
     function doreplacer(el, entry, mode, force){
         var index = -1, regex, link = getEntryLink(entry);
-        if (link && prefs.replacer_link) {
-            var indexes = [];
-            for (var i = 0, len = gp_links.length; i < len; i++) {
-                if (gp_links[i]) {
-                    //console.log(link.url);
-                    //console.log(gp_links[i]);
-                    if (re_links[i].test(link.url)) {
-                        indexes.push(i);
-                    }
-                }
-            }
-            
-            replacePart(indexes, entry, link);
+		if (link) {
+            var matches = {};
+			iterate(gp_data, function(title, o){
+				if (o.re_url.test(link.url)){
+					matches[title]=o;
+				}
+			});
+            replacePart(matches, entry, link);
         }
     }
     
-    function replacePart(indexes, entry, link){
-        if (!indexes || indexes.length===0){
+    function replacePart(matches, entry, link){
+        if (!matches || matches.length===0){
 			return false;
 		}
 		
@@ -87,10 +76,10 @@ GRP.replacer = function(prefs, langs){
             url: link.url,
             method: "GET",
             partIndex: partIndex,
-            indexes: JSON.stringify(indexes),
+            matches: JSON.stringify(matches),
             onload: function(res, req){
-                var indexes = JSON.parse(req.indexes);
-                replaceItem(res.responseText, indexes, req.partIndex);
+                var matches = JSON.parse(req.matches);
+                replaceItem(res.responseText, matches, req.partIndex);
             }
         });
     }
