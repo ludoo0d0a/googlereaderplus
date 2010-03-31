b9e447f69808079d2ab5a4ad01d3988e23f84462
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2010-03-31 16:27:53 +0000

    readitlater+instapaper = same api

---
+++ trunk/GoogleReaderPlus/src/api/readit.js
@@ -0,0 +1,96 @@
+/**
+ * Readitlater and instapaper API
+ */
+GRP.api_readit = function(prefs, langs, script, api){
+    var SL = langs[script];
+    function addButton(el, entry, mode){
+        var text = SL.text + formatShortcut(script, script, prefs); 
+        addBottomLink(el, SL.keyword, text, 'btn-' + script + ' item-star star', false, readitlaterClick, false, entry, mode);
+    }
+    function addKey(){
+        onKey('btn-' + script, readitlaterClick);
+    }
+    function readitlaterClick(btn, entry, locked){
+        //var active = isActive(btn, entry, script, locked);
+        readitlater(entry, btn);
+    }
+    function readitlater(entry, btn){
+        var auth = getAuth();
+        if (!auth) {
+            return;
+        }
+        var link = getEntryLink(entry);
+        var body = getBody(entry);
+        var params = {
+            url: link.url,
+            title: link.title,
+            selection: body.innerText
+            //selection: window.getSelection().toString()
+        };
+        //login(auth, params, btn);
+        //direct
+        post(auth, params, btn);
+    }
+    function login(auth, params, btn){
+        GM_xmlhttpRequest({
+            method: 'GET',
+            url: api.auth,
+            parameters: auth,
+            onload: function(r){
+                if (r.status == api.successCode) {
+                    //login success
+                    post(auth, params, btn);
+                } else if (api.errors[r.status]) {
+                    alert(SL[api.errors[r.status]]);
+                } else {
+                    alert(SL.error);
+                }
+            }
+        });
+    }
+    function post(auth, params, btn){
+        params.username = auth.username;
+        if (auth.password) {
+            params.password = auth.password;
+        }
+        GM_xmlhttpRequest({
+            method: 'GET',
+            url: api.add,
+            parameters: params,
+            onload: function(r){
+                if (r.status == api.successCode) {
+                    //set star active
+                    addClass(btn, 'item-star-active');
+                    removeClass(btn, 'item-star');
+                } else if (api.errors[r.status]) {
+                    alert(SL[api.errors[r.status]]);
+                } else {
+                    alert(SL.error);
+                }
+            }
+        });
+    }
+    function getAuth(){
+        var username = prefs.instapaper_username;
+        var password = prefs.instapaper_password;
+        
+if (typeof username === "undefined" || username === '') {
+            alert(SL.nologin);
+            
+            return false;
+        }
+        
+        return {
+            username: username,
+            password: password
+        };
+    }
+    function clearAuth(){
+        //GM_setValue('instapaper_username', '');
+        //GM_setValue('instapaper_password', '');
+    }
+    registerFeature(addButton, 'e' + script);
+    var keycode = getShortcutKey(script, script, prefs);
+    keycode.fn = addKey;
+    initKey(keycode);
+};
