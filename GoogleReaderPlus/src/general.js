91efd1e9ad8bb8745d3cbb61f5a04d981ef2262a
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2010-04-13 08:26:02 +0000

    float actions

--- GoogleReaderPlus/src/general.js
+++ GoogleReaderPlus/src/general.js
@@ -12,7 +12,7 @@ GRP.general = function(prefs, langs){
         }
     }
     if (prefs.general_topcurrent) {
-        var entries = get_id('entries');
+		var entries = get_id('entries');
         var h = screen.height; // getHeightEntries();
         var scrollspacer = dh(entries, 'div', {
             id: 'topcurrent',
@@ -34,7 +34,8 @@ GRP.general = function(prefs, langs){
         }, 500);
     }
 	
-	if (prefs.general_floatactions) {
+	
+	function floatactions(){
 		//Remove #entries.list .entry .entry-actions {left:0px;}
 		var ss = findre(document.styleSheets, 'href', /en-scroll\.css/);
 		if (ss) {
@@ -43,8 +44,18 @@ GRP.general = function(prefs, langs){
 				rule.style.left = '';
 			}
 		}
+		var main = get_id('main');
+		var top = main.offsetTop; //getStyle(main, 'top');
+		top = Math.max(142, top+77);
 		
-		var css = '.entry:not(#current-entry) .card-actions{display:none}#current-entry .card-actions,#entries.list #current-entry .entry-actions{position:fixed;right:32px!important;top:142px!important;left:!important;width:140px;z-index:9999;-webkit-box-shadow:#E3E5EB 0 1px 1px;border-bottom-left-radius:5px 5px;border-bottom-right-radius:5px 5px;border-top-left-radius:5px 5px;border-top-right-radius:5px 5px;border:2px solid #68E;opacity:0.2}#current-entry .card-actions:hover,#entries.list #current-entry .entry-actions:hover{opacity:1}#current-entry .entry-main{margin-right:140px}#current-entry .entry-actions > span,#entries.list #current-entry .entry-actions > span{display:block}';
+		//ig    : main.top=140 top=217 (+77)
+		//normal: main.top=65  top=142 (+77)
+		var css = '.entry:not(#current-entry) .card-actions{display:none}#current-entry .card-actions,#entries.list #current-entry .entry-actions{position:fixed;right:32px!important;top:'+top+'px!important;left:!important;width:140px;z-index:9999;-webkit-box-shadow:#E3E5EB 0 1px 1px;border-bottom-left-radius:5px 5px;border-bottom-right-radius:5px 5px;border-top-left-radius:5px 5px;border-top-right-radius:5px 5px;border:2px solid #68E;opacity:0.2}#current-entry .card-actions:hover,#entries.list #current-entry .entry-actions:hover{opacity:1}#current-entry .entry-main{margin-right:140px}#current-entry .entry-actions > span,#entries.list #current-entry .entry-actions > span{display:block}';
         GM_addStyle(css, 'rps_floatactions');
+	}
+	
+	if (prefs.general_floatactions) {
+		runfn(floatactions, 'floatactions', 99, 2000);
+		//defer to get right #main.top
     }
 };
