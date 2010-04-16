601b3a002f44383e44194da09bb70424c454ab06
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2010-04-16 12:15:17 +0000

    3.1.2

--- GoogleReaderPlus/src/version.js
+++ GoogleReaderPlus/src/version.js
@@ -2,7 +2,7 @@
  * @author Valente
  */
 window.GRP={
-	VERSION: "3.1.1",
+	VERSION: "3.1.2",
 	setVersion:function(text){
 		var ver = document.getElementById('version');
 		ver.innerHTML = (text||'') + GRP.VERSION;
