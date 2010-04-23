674b59901305d8d6049b5036c5a2b516679fcb4e
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2010-04-23 13:21:56 +0000

    slidewindow as single feature

--- GoogleReaderPlus/src/general.js
+++ GoogleReaderPlus/src/general.js
@@ -40,47 +40,6 @@ GRP.general = function(prefs, langs, ID, SL, lang){
             }
         }, 500);
     }
-    function slideWindow(){
-        var nent = prefs.general_slidewindowmini || 30, lsup = prefs.general_slidewindowmaxi || 200;
-		var c, ent = 0, bor = 0, bsig = 0, nbor = 0, centi = false;
-        var entries = get_id("entries");
-        entries.addEventListener("DOMNodeInserted", function(e){
-            if (hasClass(e.target, 'entry')) {
-				try {
-					//ent += 1;
-					ent = entries.childNodes.length;
-					if ((ent > lsup) && (!centi)) {
-						centi = true;
-						console.log(ent+'>'+lsup);
-						nbor = entries.scrollTop;
-						c = getCurrentEntry();
-						while (bor + bsig <= nbor) {
-							actual = entries.firstChild;
-							bsig = Math.floor(actual.nextSibling.offsetHeight / 2);
-							bor = bor + actual.offsetHeight;
-							console.log('remove '+actual.className);
-							entries.removeChild(actual);
-						}
-						ent = nent;
-						bor = 0;
-						entries.scrollTop = 0;
-						if (!c){
-							c = entries.firstChild;
-						}
-						//TODO: why current-entry is removed ?
-						window.setTimeout(function(){
-							selectCurrentEntry(c);
-						},300);	
-						centi = false;			
-					}
-				} catch (e) {
-				}
-			}
-        }, false);
-    }
-	if (prefs.general_slidewindow) {
-		slideWindow();
-	}
 	
     function floatactions(){
         //Remove #entries.list .entry .entry-actions {left:0px;}
