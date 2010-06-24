333889efe95b180f0ee086bef247a4f5e10c9225
ludovic.valente@gmail.com <ludovic.valente@gmail.com@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2010-06-24 08:39:12 +0000

    page action menu

---
+++ trunk/GoogleReaderPlus/src/menu/menu.js
@@ -0,0 +1,22 @@
+function initMenu(){
+    dh('menu', 'a', {
+        href: '#',
+		cls:'gb2',
+        text: 'Preferences'
+    }, {
+        click: function(){
+            GM_openInTab(mycore.getUrl('/preferences.html'));
+        }
+    });
+    dh('menu', 'a', {
+        href: '#',
+		cls:'gb2',
+        text: 'Themes'
+    }, {
+        click: function(){
+            GM_openInTab(mycore.getUrl('/preferences.html'));
+        }
+    });
+}
+
+initMenu();
