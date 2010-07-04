2c39d026d4d3e5b317d4c3bc3aaaf5c17bb7558c
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2010-07-04 21:09:33 +0000

    external guid +version button

--- trunk/GoogleReaderPlus/button/lib/external.js
+++ trunk/GoogleReaderPlus/button/lib/external.js
@@ -1,44 +1,34 @@
 /**
  * @author Valente
  */
-//*************** CORE ************************
-//prod: hhcknjkmaaeinhdjgimjnophgpbdgfmg
-//home crx : njidamgjohnfbkeagfbnkllfkdnlpjhi
-//debug : cmkepfncdncbdpmdfnkbpenhfbmmnebm
-
-//*************** ICON ************************
-//prod: ecpcafinfpjgabomoamkhkgnpgpmdmeo
-//home crx : aencokegfecfkpckmiklpcklhdblkdgj
-//unpacked : lomblngfikeinenjgnkcnhbdgchkaeai
-
-var GUID_CORE = 'hhcknjkmaaeinhdjgimjnophgpbdgfmg';
-var GUID_ICON = mycore.getGUID();
-var env = 'unpacked'; //readCookie('env');
-if (env) {
-	console.log('env toolbar=' + env);
+var GUID_CORE = 'hhcknjkmaaeinhdjgimjnophgpbdgfmg', 
+GUID_ICON_PROD = 'ecpcafinfpjgabomoamkhkgnpgpmdmeo', 
+GUID_ICON = mycore.getGUID();
+var env = ''; //readCookie('env');
+if (GUID_ICON !== GUID_ICON_PROD) {
+    env = 'unpacked';
+    console.log('env=' + env);
+    GUID_CORE = 'lnmcgabhnnfjenanodagmpjjmgbfbgje';
+    console.log('icon GUID_CORE=' + GUID_CORE);
+    console.log('icon GUID_ICON=' + GUID_ICON);
 }
-//GM_setCookieValue('env','home');
-if (env && env == 'unpacked') {
-	GUID_CORE = 'lnmcgabhnnfjenanodagmpjjmgbfbgje';
-}
-console.log('icon GUID_CORE=' + GUID_CORE);
-console.log('icon GUID_ICON=' + GUID_ICON);
-var LOCALPATH = 'chrome-extension://'+GUID_CORE;
+var LOCALPATH = 'chrome-extension://' + GUID_CORE;
 
 function call_icon(message, options, callback){
-	external_call(GUID_ICON, message, options, callback);
+    external_call(GUID_ICON, message, options, callback);
 }
+
 function call_core(message, options, callback){
-	external_call(GUID_CORE, message, options, callback);
+    external_call(GUID_CORE, message, options, callback);
 }
+
 function external_call(guid, message, options, callback){
     var emptyFn = function(){
     };
-    chrome.extension.sendRequest(guid, 
-    {
+    chrome.extension.sendRequest(guid, {
         message: message,
         options: options || {}
     }, callback || emptyFn);
     console.log('send "' + message + '" on ' + guid);
-	console.log(options);
+    console.log(options);
 }
