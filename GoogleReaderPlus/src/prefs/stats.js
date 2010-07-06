80ded991106f46fdb93d3c457f4647161f6b35d7
ludovic.valente <ludovic.valente@gmail.com>
2010-07-06 09:49:40 +0000

    stats in body

--- GoogleReaderPlus/src/prefs/stats.js
+++ GoogleReaderPlus/src/prefs/stats.js
@@ -5,18 +5,19 @@ function initstats(){
 	var _gaq = _gaq || [];
 	_gaq.push(['_setAccount', 'UA-183120-12']);
 	_gaq.push(['_trackPageview']);
+	
 	GRP = GRP || {};
 	GRP.useStats = true;
 	//console.log('track ON');
 	
 	//http://code.google.com/chrome/extensions/tut_analytics.html
-	(function(){
+	/*(function(){
 		var ga = document.createElement('script');
 		ga.type = 'text/javascript';
 		ga.async = true;
 		ga.src = 'https://ssl.google-analytics.com/ga.js';
 		(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
-	})();
+	})();*/
 }
 
 function track(name, value){
