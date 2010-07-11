8d6a0b95b49fb4875dce27111ec87bc6762717fd
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2010-07-11 23:10:41 +0000

    +oauth

---
+++ trunk/GoogleReaderPlus/button/contacts.js
@@ -0,0 +1,80 @@
+/*
+ * oauth sample
+ * http://code.google.com/chrome/extensions/trunk/tut_oauth.html
+ * 
+ * http://src.chromium.org/viewvc/chrome/trunk/src/chrome/common/extensions/docs/examples/extensions/oauth_contacts/
+ * 
+ */
+var oauth = ChromeExOAuth.initBackgroundPage({
+    'request_url': 'https://www.google.com/accounts/OAuthGetRequestToken',
+    'authorize_url': 'https://www.google.com/accounts/OAuthAuthorizeToken',
+    'access_url': 'https://www.google.com/accounts/OAuthGetAccessToken',
+    'consumer_key': 'anonymous',
+    'consumer_secret': 'anonymous',
+    'scope': 'http://www.google.com/m8/feeds/',
+    'app_name': 'Readerplus - OAuth Contacts'	
+});
+
+
+var contacts = null;
+
+function setIcon(){
+    if (oauth.hasToken()) {
+        chrome.browserAction.setIcon({
+            'path': 'images/19.png'
+        });
+    } else {
+        chrome.browserAction.setIcon({
+            'path': 'images/19-gray.png'
+        });
+    }
+};
+
+function onContacts(text, xhr){
+    contacts = [];
+    var data = JSON.parse(text);
+    for (var i = 0, entry; entry = data.feed.entry[i]; i++) {
+        var contact = {
+            'name': entry['title']['$t'],
+            'id': entry['id']['$t'],
+            'emails': []
+        };
+        
+        for (var j = 0, email; email = entry['gd$email'][j]; j++) {
+            contact['emails'].push(email['address']);
+        }
+        
+        if (!contact['name']) {
+            contact['name'] = contact['emails'][0] || "<Unknown>";
+        }
+        contacts.push(contact);
+    }
+    
+    chrome.tabs.create({
+        'url': 'contacts.html'
+    });
+};
+
+function getContacts(){
+    oauth.authorize(function(){
+        console.log("on authorize");
+        setIcon();
+        var url = "http://www.google.com/m8/feeds/contacts/default/full";
+        oauth.sendSignedRequest(url, onContacts, {
+            'parameters': {
+                'alt': 'json',
+                'max-results': 100
+            }
+        });
+    });
+};
+
+function logout(){
+    oauth.clearTokens();
+    setIcon();
+};
+
+setIcon();
+chrome.browserAction.onClicked.addListener(getContacts);
+//setpopup('popup.html')
+
