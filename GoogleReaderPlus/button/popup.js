317f6a00567ca395ee328a76fea5c54b34beda10
ludovic.valente <ludovic.valente@gmail.com>
2010-07-13 15:40:18 +0000

    filter by feed

--- GoogleReaderPlus/button/popup.js
+++ GoogleReaderPlus/button/popup.js
@@ -7,10 +7,10 @@ var g = chrome.i18n.getMessage;
 var opendirect = bgPage.getPref('opendirect');
 if (opendirect) {
     mycore.extension.sendRequest({
-			message: 'core',
-			action:'findreader'
-	});
-	window.close();
+        message: 'core',
+        action: 'findreader'
+    });
+    window.close();
 } else {
     $(document).ready(startDom);
 }
@@ -23,12 +23,12 @@ g("starred_items") +
 g("mark_as_read") +
 '"></span>' +
 '<div class="entry-title">' +
-'<img class="entry-favicon {iconcls}" title="{origin}" src="{icon}">' +
+'<img class="entry-favicon {iconcls}" feed="{feed}" title="{origin}" src="{icon}">' +
 '<a class="entry-original" target="_blank" href="{url}"{atitle}>{title}</a>' +
 '</div>' +
 '</div>';
 function startDom(){
-	$('.i18n').each(function(i, el){
+    $('.i18n').each(function(i, el){
         var t = g(el.id);
         if (t) {
             $(el).text(t);
@@ -38,16 +38,16 @@ function startDom(){
     //Fill data feed
     loaditems(1, true);
     
-	$('#loptions').attr('title', g('needreaderplus'));
+    $('#loptions').attr('title', g('needreaderplus'));
     $('#loptions').click(function(){
         if (!$(this).hasClass('link-off')) {
             mycore.extension.sendRequest({
-					message: 'core',
-					action:'openprefs'
-			});
+                message: 'core',
+                action: 'openprefs'
+            });
         }
     });
-	
+    
     $('#lrefresh').click(function(){
         loaditems(1, true);
     });
@@ -66,41 +66,50 @@ function startDom(){
             message: 'contacts'
         });
     });
-	
-	//$(document).keypress(function(e){
-	$(document).keydown(function(e){
-		var w = e.keyCode;
+    
+    //$(document).keypress(function(e){
+    $(document).keydown(function(e){
+        var w = e.keyCode;
         if (w == 39 || w == 40 || w == 34) {
             //right,down,pagedown
-			if (!$('#lnext').hasClass('link-off')) {
-	            loaditems(++page);
-	        }
-        }else if (w == 37 || w == 38 || w == 33) {
+            if (!$('#lnext').hasClass('link-off')) {
+                loaditems(++page);
+            }
+        } else if (w == 37 || w == 38 || w == 33) {
             //left,up,pageup
-			if (!$('#lprevious').hasClass('link-off')) {
-	            loaditems(--page);
-	        }
+            if (!$('#lprevious').hasClass('link-off')) {
+                loaditems(--page);
+            }
         };
-    });
+            });
     
 }
 
 var URL_FAVICON = "http://pageicons.appspot.com/favicons?url=";
 var page = 1, pageItems = 10, items = {};
-function loaditems(page, reload){
+function loaditems(page, reload, feed, label){
     msg(g('loadingpage', [page]));
     if (reload) {
         mycore.extension.sendRequest({
             message: 'unread',
+            label: label || '',
+            feed: (feed)?feed.feed:'',
+			feed_title: (feed)?feed.title:'',
             count: 100
         }, function(o){
-            if (o && o.items) {
-                items = o.items;
-                displayEntries(items, page);
-				if (o.GUID_CORE) {
-					//ReaderPlus detected
-					$('#loptions').removeClass('link-off').attr('title', '');
-				}
+            if (o) {
+                if (label && o.entry) {
+                    //from xml
+                    items = o.entry;
+                    displayEntries(items, page, feed, label);
+                } else if (o.items) {
+                    items = o.items;
+                    displayEntries(items, page, feed, label || '');
+                }
+                if (o.GUID_CORE) {
+                    //ReaderPlus detected
+                    $('#loptions').removeClass('link-off').attr('title', '');
+                }
             } else {
                 //not logged
                 msg(g('notlogged'));
@@ -119,12 +128,20 @@ function msg(txt){
 var reCategory = /user\/\d+\/state\/com\.google\/(.*)/;
 var reLabel = /user\/\d+\/label\/(.*)/;
 
-function displayEntries(items, page){
+function displayEntries(items, page, feed, label){
     page = page || 1;
     page = Math.max(page, 1);
     var pagesMax = items ? Math.round(items.length / pageItems) : 1;
     page = Math.min(page, 10);
     $('#nav').html(g('page', [page, pagesMax]));
+    
+    if (feed){
+		$('#htitle').text(g('feeditems', [feed.title]));
+	}else if (label) {
+        $('#htitle').text(g('labelitems', [label]));
+    } else {
+        $('#htitle').text(g('unreaditems'));
+    }
     
     toggleClass($('#lnext'), 'link-on', 'link-off', page < pagesMax);
     toggleClass($('#lprevious'), 'link-on', 'link-off', page > 1);
@@ -135,15 +152,17 @@ function displayEntries(items, page){
         if (index < min || index > max) {
             return;
         }
-        var url = item.alternate[0].href;
+        var url = '';
+        if (item.alternate) {
+            url = item.alternate[0].href;
+        } else if (item.source && item.source.link) {
+            url = item.source.link['@href'];
+        }
         item.actions = item.actions || {};
         var starred = item.actions.starred || false, read = item.actions.read || false, categories = [], labels = [];
-        $.each(item.categories, function(i, cat){
+        $.each(item.categories || item.category, function(i, category){
+            var cat = category['@term'] || category;
             var m = reCategory.exec(cat);
-            if (cat.indexOf('starred') > 0) {
-                cat = cat;
-            }
-            
             if (m && m[1]) {
                 categories.push(m[1]);
                 if ('starred' === m[1]) {
@@ -166,10 +185,10 @@ function displayEntries(items, page){
             date: '', //item.published,
             url: url,
             icon: ((item.enclosure && item.enclosure.href) ? item.enclosure.href : getFavicon(url)),
-            //title: ellipsis(item.title, 85),
             title: item.title,
             summary: '',
-            origin: (item.origin) ? item.origin.title : '',
+            feed: (item.origin) ? item.origin.streamId : '',
+            origin: (item.origin) ? item.origin.title : ((item.source) ? item.source.title : ''),
             //categories:categories,
             //labels:labels,
             starcls: (starred) ? 'item-star-active' : 'item-star',
@@ -179,6 +198,9 @@ function displayEntries(items, page){
             a.summary = cleanHTML(item.summary.content);
         } else if (item.content && item.content.content) {
             a.summary = cleanHTML(item.content.content);
+        } else if (item.summary) {
+            //label
+            a.summary = cleanHTML(item.summary);
         }
         
         a.atitle = (a.summary) ? (' title="' + a.summary + '"') : '';
@@ -228,6 +250,16 @@ function displayEntries(items, page){
      $('.entry-original').click(markasread);
      */
     tooltips();
+    
+    $('.entry-favicon').click(function(){
+        var feed = $(this).attr('feed');
+        if (feed) {
+            loaditems(1, true, {
+                feed: feed,
+                title: $(this).attr('title')
+            });
+        }
+    });
 }
 
 function tooltips(){
