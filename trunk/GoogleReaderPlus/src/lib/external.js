628f4a0d00315485a9c0605e97e3818f5b9c5a18
ludovic.valente@gmail.com <ludovic.valente@gmail.com@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2010-07-13 14:28:21 +0000

    fixlayout ss add subscription remove read items

--- trunk/GoogleReaderPlus/src/lib/external.js
+++ trunk/GoogleReaderPlus/src/lib/external.js
@@ -3,16 +3,12 @@
  */
 var GUID_CORE = mycore.getGUID(), 
 GUID_CORE_PROD = 'hhcknjkmaaeinhdjgimjnophgpbdgfmg', 
-GUID_ICON = 'ecpcafinfpjgabomoamkhkgnpgpmdmeo';
-var env = ''; //readCookie('env');
+GUID_ICON = 'ecpcafinfpjgabomoamkhkgnpgpmdmeo',
+LOCALPATH = mycore.getLocalPath();
+
 if (GUID_CORE!==GUID_CORE_PROD){
-	env='unpacked';
-	console.log('env=' + env);
-	GUID_ICON = 'mimnpgjgngdgepfmgegapflddkjddkde';
-	console.log('GUID_CORE=' + GUID_CORE);
-	console.log('GUID_ICON=' + GUID_ICON);
+	GUID_ICON = 'cgpgjbahhnkejmclppnpkcoildokbmem';
 }
-var LOCALPATH = mycore.getLocalPath();
 
 function call_icon(message, options, callback){
 	external_call(GUID_ICON, message, options, callback);
@@ -21,11 +17,13 @@ function call_core(message, options, callback){
 	external_call(GUID_CORE, message, options, callback);
 }
 function external_call(guid, message, options, callback){
-    var emptyFn = function(){
+    console.log('CORE external_call ['+message+'] to '+guid);
+	var emptyFn = function(){
     };
     mycore.extension.sendRequest(guid, 
     {
         message: message,
+		keypass: "##ReaderPlus",
         options: options || {}
     }, callback || emptyFn);
     //console.log('send "' + message + '" on ' + guid);
