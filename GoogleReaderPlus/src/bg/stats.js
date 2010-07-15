26c2793b7001ca33314ed9ad898dfea737973adf
ludovic.valente <ludovic.valente@gmail.com>
2010-07-15 13:09:39 +0000

    -logs tracker

--- GoogleReaderPlus/src/bg/stats.js
+++ GoogleReaderPlus/src/bg/stats.js
@@ -25,13 +25,12 @@ function track(name, value){
         value = name.value;
         name = name.name;
     }
-    console.log('try track [GRP.useStats='+GRP.useStats+']' + name + '=' + value);
     if (GRP.useStats && _gaq) {
 		if (typeof name === 'object') {
             value = name.value;
             name = name.name;
         }
         _gaq.push(['_trackEvent', name, value]);
-        console.log('track '+name+':'+value);
+        //console.log('track '+name+':'+value);
     }
 }
