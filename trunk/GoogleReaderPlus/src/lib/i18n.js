c9a29f98db8491002013015bccf6f945d1ec6756
ludovic.valente@gmail.com <ludovic.valente@gmail.com@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2010-07-15 13:01:01 +0000

    i18n fixes

--- trunk/GoogleReaderPlus/src/lib/i18n.js
+++ trunk/GoogleReaderPlus/src/lib/i18n.js
@@ -2,94 +2,94 @@
  * @author Valente
  */
 function getText(lang, script, option, deflang, deftext){
-	var text = '';
-	if (GRP.langs[lang] && GRP.langs[lang].texts[script] && GRP.langs[lang].texts[script][option]) {
+    var text = '';
+    if (GRP.langs[lang] && GRP.langs[lang].texts[script] && GRP.langs[lang].texts[script][option]) {
         text = GRP.langs[lang].texts[script][option];
     }
-	if (!text && deflang){
-		text=getText(deflang, script, option, false, deftext);
-	}
-	if (!text && !deflang) {
-		text=deftext||'';
-	}
-	return text;
+    if (!text && deflang) {
+        text = getText(deflang, script, option, false, deftext);
+    }
+    if (!text && !deflang) {
+        text = deftext || '';
+    }
+    return text;
 }
 
 function getCategory(lang, name, deflang){
-	var text = '';
-	if (GRP.langs[lang] && GRP.langs[lang].categories && GRP.langs[lang].categories[name]) {
+    var text = '';
+    if (GRP.langs[lang] && GRP.langs[lang].categories && GRP.langs[lang].categories[name]) {
         text = GRP.langs[lang].categories[name];
     }
-	if (!text && deflang){
-		text=getCategory(deflang, name);
-	}
-	if (!text && !deflang) {
-		text=name.toMaj();
-	}
-	return text;
+    if (!text && deflang) {
+        text = getCategory(deflang, name);
+    }
+    if (!text && !deflang) {
+        text = name.toMaj();
+    }
+    return text;
 }
 
 
 function getTextPrefs(lang, script, option, deflang, deftext){
-	var text = '';
-	if (GRP.langs[lang] && GRP.langs[lang].prefs[script] && GRP.langs[lang].prefs[script][option]) {
+    var text = '';
+    if (GRP.langs[lang] && GRP.langs[lang].prefs[script] && GRP.langs[lang].prefs[script][option]) {
         text = GRP.langs[lang].prefs[script][option];
     }
-	if (!text && deflang){
-		text=getTextPrefs(deflang, script, option, false, deftext);
-	}
-	if (!text && !deflang) {
-		text=deftext||'';
-	}	
-	return text;
+    if (!text && deflang) {
+        text = getTextPrefs(deflang, script, option, false, deftext);
+    }
+    if (!text && !deflang) {
+        text = deftext || '';
+    }
+    return text;
 }
 
 
 function autoTranslate(name){
     var params = urlDecode(window.location.search.substring(1));
     var lang = params.lang || 'en';
-	loadLangs(lang, function(){
-		translatePage(lang, name);
-	});
+    loadLangs(lang, function(){
+        translatePage(lang, name);
+    });
 }
-/*
-//Dynamical translation (too complex ; not useful)
-function onLang(){
-	var lang = get_id('language_lang').value; 
-	loadLangs(lang, function(){
-		translatePage(lang, 'cat', 'categories',0);
-		translatePage(lang);
-	});
-}
-*/
 
+/*
+ //Dynamical translation (too complex ; not useful)
+ function onLang(){
+ var lang = get_id('language_lang').value;
+ loadLangs(lang, function(){
+ translatePage(lang, 'cat', 'categories',0);
+ translatePage(lang);
+ });
+ }
+ */
 function translatePage(lang, name, section, level){
-	section=section||'prefs';
-	var translations= GRP.langs[lang][section];
+    section = section || 'prefs';
+    var translations = GRP.langs[lang][section];
     if (translations) {
-		function replaceTexts(name, texts){
-			for (var key in texts) {
-				var id = 't_' + ((name === 'global') ? '' : (name + '_')) + key;
-				var el = document.getElementById(id);
-				if (el) {
-					var text = texts[key];
-					if (/^val-/.test(key)) {
-						el.value = text;
-					} else {
-						el.innerHTML = text;
-					}
-				}
-			}
-		}
-        if (name){
-			var texts = (level==0)?translations:translations[name];
-			replaceTexts(name, texts);
-		}else{
-			for (var nam in translations) {
-				var texts = translations[nam];
-				replaceTexts(nam, texts);
-			}
-		}
+        function replaceTexts(name, texts){
+            for (var key in texts) {
+                var id = 't_' + ((name === 'global') ? '' : (name + '_')) + key;
+                var el = document.getElementById(id);
+                if (el) {
+                    var text = texts[key];
+                    if (/^val-/.test(key)) {
+                        el.value = text;
+                    } else {
+                        el.innerHTML = text;
+                    }
+                }
+            }
+        }
+        if (name) {
+            var texts = (level == 0) ? translations : translations[name];
+            replaceTexts(name, texts);
+        } else {
+            for (var nam in translations) {
+                var texts = translations[nam];
+                replaceTexts(nam, texts);
+            }
+        }
     }
 }
 
@@ -130,21 +130,21 @@ function splitId(id){
 
 function loadLangs(lang, cb, scope){
     //TODO: load json here instead evald js
-	if (!GRP.langs[lang]) {
-		GM_addScript('lang/' + lang + '/features.js', true, function(){
-			GM_addScript('lang/' + lang + '/langs.js', true, function(){
-				//override locale texts
-				if (GRP.langs) {
-					merge(GRP, GRP.langs[lang]);
-				}
-				if (cb) {
-					cb.call(scope || this);
-				}
-			}, true);
-		}, true);
-	}else{
-		if (cb) {
-					cb.call(scope || this);
-				}
-	}
+    if (!GRP.langs[lang]) {
+        GM_addScript('lang/' + lang + '/features.js', true, function(){
+            GM_addScript('lang/' + lang + '/langs.js', true, function(){
+                //override locale texts
+                if (GRP.langs) {
+                    merge(GRP, GRP.langs[lang]);
+                }
+                if (cb) {
+                    cb.call(scope || this);
+                }
+            }, true);
+        }, true);
+    } else {
+        if (cb) {
+            cb.call(scope || this);
+        }
+    }
 }
