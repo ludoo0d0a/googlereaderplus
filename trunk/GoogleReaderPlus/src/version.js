6761408d9102f52e2cb8218dce81f9a347fba9eb
ludovic.valente@gmail.com <ludovic.valente@gmail.com@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2010-09-03 13:28:09 +0000

    3.4.2 version

--- trunk/GoogleReaderPlus/src/version.js
+++ trunk/GoogleReaderPlus/src/version.js
@@ -2,7 +2,7 @@
  * @author Valente
  */
 window.GRP={
-	VERSION: "3.4.0",
+	VERSION: "3.4.2",
 	setVersion:function(text){
 		var ver = document.getElementById('version');
 		ver.innerHTML = (text||'') + GRP.VERSION;
