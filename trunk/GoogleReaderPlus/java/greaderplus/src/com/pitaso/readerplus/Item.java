751d21f4741385f76043a2f55993f857230f5ec5
ludovic.valente@gmail.com <ludovic.valente@gmail.com@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2011-02-03 09:52:57 +0000

    cached mirror

---
+++ trunk/GoogleReaderPlus/java/greaderplus/src/com/pitaso/readerplus/Item.java
@@ -0,0 +1,21 @@
+package com.pitaso.readerplus;
+
+public class Item {
+
+	private String name;
+	private Data data;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Data getData() {
+		return data;
+	}
+	public void setData(Data data) {
+		this.data = data;
+	}
+	
+	
+}
