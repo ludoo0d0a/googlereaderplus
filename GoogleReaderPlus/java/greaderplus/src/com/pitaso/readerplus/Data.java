141a09361017112c9e4905ce58569fc981f03324
ludovic.valente <ludovic.valente@gmail.com>
2011-02-03 09:52:57 +0000

    cached mirror

---
+++ GoogleReaderPlus/java/greaderplus/src/com/pitaso/readerplus/Data.java
@@ -0,0 +1,28 @@
+package com.pitaso.readerplus;
+
+public class Data {
+
+	private String search;
+	private String replace;
+	private String url;
+	public String getSearch() {
+		return search;
+	}
+	public void setSearch(String search) {
+		this.search = search;
+	}
+	public String getReplace() {
+		return replace;
+	}
+	public void setReplace(String replace) {
+		this.replace = replace;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	
+}
