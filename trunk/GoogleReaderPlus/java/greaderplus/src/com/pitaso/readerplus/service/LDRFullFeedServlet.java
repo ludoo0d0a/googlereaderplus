5f0ab9f39da22284e1e6c4b569a0273fc1889a85
ludovic.valente@gmail.com <ludovic.valente@gmail.com@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2011-02-03 16:29:41 +0000

    icon +LDRFullFeed +queue

---
+++ trunk/GoogleReaderPlus/java/greaderplus/src/com/pitaso/readerplus/service/LDRFullFeedServlet.java
@@ -0,0 +1,43 @@
+package com.pitaso.readerplus.service;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.pitaso.readerplus.pojo.ItemLDRFullFeed;
+
+public class LDRFullFeedServlet extends AbstractWeDataServlet{
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3438080955825682063L;
+	private static final String KEY_REPLACER = "LDRFullFeed";
+	private static final String DATABASE = "LDRFullFeed";
+	
+	@Override
+	public String getDatabase() {
+		return DATABASE;
+	}
+	
+	@Override
+	public String getKey() {
+		return KEY_REPLACER;
+	}
+	
+	@Override
+	public String compactJson(String json) {
+		Type listType = new TypeToken<List<ItemLDRFullFeed>>() {}.getType();
+		List<ItemLDRFullFeed> items = new Gson().fromJson(json, listType);
+		
+		//Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		Gson gson = new GsonBuilder().create();
+		String out = gson.toJson(items);
+		return out;
+	}
+	
+	
+}
---
+++ trunk/GoogleReaderPlus/java/greaderplus/src/com/pitaso/readerplus/service/ReplacerServlet.java
@@ -0,0 +1,43 @@
+package com.pitaso.readerplus.service;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.pitaso.readerplus.pojo.ItemReplacer;
+
+public class ReplacerServlet extends AbstractWeDataServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8930935362394876465L;
+	
+	private static final String KEY_REPLACER = "replacer";
+	private static final String DATABASE = "Replacer";
+	
+	@Override
+	public String getDatabase() {
+		return DATABASE;
+	}
+	
+	@Override
+	public String getKey() {
+		return KEY_REPLACER;
+	}
+	
+	@Override
+	public String compactJson(String json) {
+		Type listType = new TypeToken<List<ItemReplacer>>() {}.getType();
+		List<ItemReplacer> items = new Gson().fromJson(json, listType);
+		
+		//Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		Gson gson = new GsonBuilder().create();
+		String out = gson.toJson(items);
+		return out;
+	}
+	
+	
+}
