a28fd6eb9147f5f7d2de5093998de51edc785e32
ludovic.valente@gmail.com <ludovic.valente@gmail.com@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2011-02-04 09:59:58 +0000

    rebuilding recache (using cron and queue)

---
+++ trunk/GoogleReaderPlus/java/greaderplus/src/com/pitaso/readerplus/service/QueueRecacheServlet.java
@@ -0,0 +1,40 @@
+package com.pitaso.readerplus.service;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
+import javax.cache.Cache;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.pitaso.readerplus.pojo.ItemReplacer;
+
+public class QueueRecacheServlet extends AbstractWeDataServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6956785182037586376L;
+
+	@Override
+	public String getDatabase() {
+		return null;
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+	throws IOException {
+		resp.setContentType("application/json");
+		resp.setCharacterEncoding("UTF-8");
+		
+		String database = req.getParameter("database");
+		populate(database);
+		resp.getWriter().println("[]");
+	}
+	
+	
+	
+}
