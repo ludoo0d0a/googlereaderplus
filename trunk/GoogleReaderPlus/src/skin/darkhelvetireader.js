79871d952f8beb29f7336fd8517f4d79e565a923
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2011-02-27 20:10:34 +0000

    uncomment all css

--- trunk/GoogleReaderPlus/src/skin/darkhelvetireader.js
+++ trunk/GoogleReaderPlus/src/skin/darkhelvetireader.js
@@ -9,7 +9,7 @@ GRP.darkhelvetireadernight = function(){
 }
 GRP.darkhelvetireader = function(theme){
     var base = 'https://github.com/ankit/flavored-hreader/raw/master/';
-    var styles = ["css/helvetireader.css"];//, "css/scrollbars.css", "css/sprites.css", "css/light-theme.css", "css/dark-theme.css", "css/buttons.css"];
+    var styles = ["css/helvetireader.css", "css/scrollbars.css", "css/sprites.css", "css/light-theme.css", "css/dark-theme.css", "css/buttons.css"];
     
 	//TODO concat CSS to work toggle on whole feature
 	addClass(document.body, (theme=='day')?'HelvetiDay':'HelvetiNight'); 
@@ -24,22 +24,4 @@ GRP.darkhelvetireader = function(theme){
 	GM_addStyle(css, 'rpsfix_darkhelvetireader');
 	
     fireResize();
-    
-    function toggleView(){
-        var display = document.getElementById("nav").style.display;
-        var status = (display == 'none') ? 1 : 0; //1 indicates fullscreen
-        if (!status) {
-            document.getElementById("nav").style.display = 'none';
-            document.getElementById("search").style.display = 'none';
-            document.getElementById("chrome").style.margin = "0px !important";
-            document.getElementById("logo-container").style.display = 'none';
-            document.getElementById("main").style.top = "15px";
-        } else {
-            document.getElementById("nav").style.display = 'block';
-            document.getElementById("search").style.display = 'block';
-            document.getElementById("chrome").style.margin = "-50px 0 10px 245px !important";
-            document.getElementById("main").style.top = "65px";
-        }
-    }
-    
 };
