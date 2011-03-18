3c58c75c1f20e7f785258b74198fa0540b7aa70b
ludovic.valente@gmail.com <ludovic.valente@gmail.com@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2011-03-18 10:07:11 +0000

    addthis updates

--- trunk/GoogleReaderPlus/src/addthis.js
+++ trunk/GoogleReaderPlus/src/addthis.js
@@ -17,14 +17,17 @@ GRP.addthis = function(prefs, langs, ID, SL, lang){
 	var BTN_234 = '<a class="addthis_button_preferred_2"></a><a class="addthis_button_preferred_3"></a><a class="addthis_button_preferred_4"></a>';
 	var LAYOUTS = {
 		button_classic:		 START_LINK+' class="addthis_button">{text}</a>',
+		button_menu:	
+		{	tpl: 	START_LINK+' class="addthis_button">{text}</a>',
+			cfg: 
+		},
 		button:   			 LINK,
 		toolbox:  			 CTN_TB+'{html}</div>',
 		toolbox_button_icons:CTN_TB+LINK+'<span class="addthis_separator">|</span><a class="addthis_button_preferred_1"></a>'+BTN_234+'</div>',
 		toolbox_button:      CTN_TB+LINK+'</div>',
 		toolbox_icons:       '<a target="_addthis" id="{nid}" class="addthis_button_preferred_1"></a>'+BTN_234+'<a class="addthis_button_compact"></a></div>',
 		button_text_icons:	 HEADER_BTN+'<img src="http://s7.addthis.com/static/btn/v2/lg-share-'+lang+'.gif" width="125" height="16" alt="Bookmark and Share" style="border:0"/></a>',
-		button_text:		 HEADER_BTN+'<img src="http://s7.addthis.com/static/btn/sm-share-'+lang+'.gif" width="83" height="16" alt="Bookmark and Share" style="border:0"/></a>',
-		button_tweet:	 START_LINK+' class="addthis_inline addthis_button_tweet"'+ATTRS+'>'
+		button_text:		 HEADER_BTN+'<img src="http://s7.addthis.com/static/btn/sm-share-'+lang+'.gif" width="83" height="16" alt="Bookmark and Share" style="border:0"/></a>'
 	};
 	var api = {
         shortcut:'addthis',
@@ -40,10 +43,15 @@ GRP.addthis = function(prefs, langs, ID, SL, lang){
 	console.log('addthis - add core');
 	GM_addjs('http://s7.addthis.com/js/250/addthis_widget.js#pubid='+api.cfg.pubid, false, 'grp_addthis');
 	//GM_addjs('http://googlereaderplus.googlecode.com/svn/trunk/GoogleReaderPlus/test/addthis/addthis_widget-debug.js#pubid='+api.cfg.pubid, false, 'grp_addthis');
-			
+		
 	var layout = prefs[ID+'_layout'] || 'button_classic';
 	console.log('layout='+layout);
-
+	
+	var ol=LAYOUTS[layout] || {};
+	if (typeof ol !=='object'){
+		ol = {tpl: layout};
+	}
+	
 	function renderIcons(){
 		var html='',svcs = {email: 'Email', print: 'Print', facebook: 'Facebook', expanded: 'More'};
 		for (var s in svcs) {
@@ -54,14 +62,15 @@ GRP.addthis = function(prefs, langs, ID, SL, lang){
 	
 	var btn_inc=0;
 	function addButtonLink(el, entry, mode, title, text){
-		var method='',btncls='', link = getEntryLink(entry);
+		var method='',btncls='', link = getEntryLink(entry), classic = (layout === 'button_classic');
 		var o = api.cfg;
 		o.text = o.text; //text
 		o.url = escapeJson(link.url);
 		o.title = escapeJson(link.title);
 		o.desc = '';
 		o.html='Share';
-		if (layout === 'button_classic') {
+		
+		if (classic) {
 			btncls = 'btn-addthis item-star star link unselectable';
 		}
 		o.nid = 'btn-addthis-'+btn_inc++;
@@ -76,22 +85,25 @@ GRP.addthis = function(prefs, langs, ID, SL, lang){
 		var title = SL.text + formatShortcut(ID, 'share', prefs); //[b]
 		var sp= dhc({root:el, tag: 'span',cls:btncls, html: html, title:title});
 		var script=''; //"if(!addthis){console.error('addthis not found');return;}";
-		//script+="addthis.{method}('#"+o.nid+"', {}, {'url':'{url}', 'title':'{title}'});";
-		script+="console.log('go_"+o.nid+"');";
-		script+="console.log('addthis='+addthis);";
-		script+="console.log('addthis.{method}='+(addthis && addthis.{method}));";
-		script+="addthis.{method}('#"+o.nid+"');";
-		script+="console.log('stop');";
+		var cfg = '{}';
+		if (layout)
+		script+="addthis.{method}('#"+o.nid+"', '+cfg+', {'url':'{url}', 'title':'{title}'});";
+		//script+="console.log('go_"+o.nid+"');";
+		//script+="console.log('addthis='+addthis);";
+		//script+="console.log('addthis.{method}='+(addthis && addthis.{method}));";
+		//script+="addthis.{method}('#"+o.nid+"');";
+		//script+="console.log('stop');";
 		script = fillTpl(script, o);
 		//console.log(script);
-		setTimeout(function(){
-			//addthis[o.method]('#"+o.nid+"');
-			GM_addjs(script, true, 's_'+o.nid, false, false, false, sp);
-		},1000);
+		if (!classic) {
+			//setTimeout(function(){
+			GM_addjs(script, true, 's_' + o.nid, false, false, false, sp);
+			//},1000);
+		}
 	}
 	
 	api.button = addButtonLink;
-	api.tpl=LAYOUTS[layout]+api.tpl;
+	api.tpl=ol.tpl+api.tpl;
 	
     GRP.api_micro(prefs, langs, ID, SL, lang, api);
 };
