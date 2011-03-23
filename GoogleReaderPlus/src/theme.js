7ac0d152e1eda6240135838dfe45ecc514d1ed08
ludovic.valente <ludovic.valente@gmail.com>
2011-03-23 14:59:58 +0000

    resize moved in callback

--- GoogleReaderPlus/src/theme.js
+++ GoogleReaderPlus/src/theme.js
@@ -1,6 +1,6 @@
 // Theme
 GRP.theme = function(prefs, langs, ID, SL, lang, scop){
-	if (prefs.theme_noborder) {
+    if (prefs.theme_noborder) {
         var css = '.card-common,.entry-main{margin:0 !important;}';
         css += '.entry,.card-content,.entry-actions,.entry-container{padding:0 !important;}';
         css += '.entry-title{margin-left:20px !important;font-size:120% !important;}';
@@ -12,11 +12,11 @@ GRP.theme = function(prefs, langs, ID, SL, lang, scop){
         css += '.entry:nth-child(even) .card-actions {background-color:transparent;}';
         GM_addStyle(css, 'rps_noborder');
     }
-	
-	var skin = GRP.skins[prefs.theme_skin];
+    
+    var skin = GRP.skins[prefs.theme_skin];
     if (!skin) {
-        console.log('Error with skin '+prefs.theme_skin);
-		return;
+        console.log('Error with skin ' + prefs.theme_skin);
+        return;
     }
     skin.id = prefs.theme_skin;
     if (skin.url) {
@@ -25,81 +25,77 @@ GRP.theme = function(prefs, langs, ID, SL, lang, scop){
     } else {
         scop.run(skin.id, langs);
     }
-	
-	var tog={};
-	function toggleTheme(){
-		var el = get_id('rps_' + skin.id);
-		if (el) {
-			tog = {
-				css: el.innerHTML,
-				src:el.href
-			};
-			remove(el);
-			/*if (scop.toggle) {
-				scop.toggle(skin.id, false);
-			}*/
-		} else if(tog.css){
-			GM_addStyle(tog.css, 'rps_' + skin.id);
-			/*if (scop.toggle) {
-				scop.toggle(skin.id, true);
-			}*/
-		} else if(tog.src){
-			GM_addCss(tog.src, 'rps_' + skin.id);
-			/*if (scop.toggle) {
-				scop.toggle(skin.id, true);
-			}*/
-		}
-		if (skin.resize) {
-			fireResize(skin.resize);
-		}
-	}
-	
+    
+    var tog = {};
+    function toggleTheme(){
+        var el = get_id('rps_' + skin.id);
+        if (el) {
+            tog = {
+                css: el.innerHTML,
+                src: el.href
+            };
+            remove(el);
+        } else if (tog.css) {
+            GM_addStyle(tog.css, 'rps_' + skin.id);
+            if (skin.resize) {
+	            fireResize(skin.resize);
+	        }
+        } else if (tog.src) {
+            GM_addCss(tog.src, 'rps_' + skin.id);
+           if (skin.resize) {
+            fireResize(skin.resize);
+        	}
+        }
+    }
+    
     function remoteSkin(skin){
         var c = GM_getValue('cache_theme_' + skin.id);
         if (c) {
             GM_addStyle(c, 'rps_' + skin.id);
+            if (skin.resize) {
+                fireResize(skin.resize);
+            }
         } else {
             GM_xmlhttpRequest({
                 method: 'get',
                 url: skin.url,
                 onload: function(r){
                     var css = r.responseText;
-					//unescape
+                    //unescape
                     css = css.replace(/\\n/g, ' ').replace(/\\"/g, '"');
                     //Filter userscripts userjs
                     if (/^http\:\/\/userstyles\.org/.test(skin.url)) {
                         var m = /var\s+css\s+=\s+"(.*?)";\n/.exec(css);
                         if (m && m[1]) {
                             css = m[1];
-                        }else{
-							//not better?
-							m = /css\s+\+=\s+"body(.*?)";\n/.exec(css);
-							if (m && m[1]) {
-								css = 'body'+m[1];
-							}
-						}
+                        } else {
+                            //not better?
+                            m = /css\s+\+=\s+"body(.*?)";\n/.exec(css);
+                            if (m && m[1]) {
+                                css = 'body' + m[1];
+                            }
+                        }
                     }
                     if (css) {
                         css = css.replace(/\n/g, ' ');
-						css = css.replace(/\-moz\-/g, '-webkit-');
-						if (skin.fix){
-							css+=skin.fix;
-						}
-						GM_addStyle(css, 'rps_' + skin.id);
+                        css = css.replace(/\-moz\-/g, '-webkit-');
+                        if (skin.fix) {
+                            css += skin.fix;
+                        }
+                        GM_addStyle(css, 'rps_' + skin.id);
                         //cache css 
-						//param3 = false : force local storage in webpage
-						GM_setValue('theme_' + skin.id, css, false);
+                        //param3 = false : force local storage in webpage
+                        GM_setValue('theme_' + skin.id, css, false);
+                        if (skin.resize) {
+                            fireResize(skin.resize);
+                        }
                     }
                 }
             });
         }
     }
-    	
-	if (skin.resize) {
-		fireResize(skin.resize);
-	}
-	
-	var keycode = getShortcutKey(ID, 'toggletheme', prefs); 
+    
+    var keycode = getShortcutKey(ID, 'toggletheme', prefs);
     if (keycode) {
         keycode.fn = toggleTheme;
         initKey(keycode);
