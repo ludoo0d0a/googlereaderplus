cfd3d5a73ffb63a9c11b96459e66520fe148150e
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2011-06-21 14:55:16 +0000

    cleanup

--- GoogleReaderPlus/src/bg/auth.js
+++ GoogleReaderPlus/src/bg/auth.js
@@ -23,7 +23,7 @@ var OAUTHS = {
 	        'authorize_url': 'https://www.google.com/accounts/OAuthAuthorizeToken',
 	        'access_url': 'https://www.google.com/accounts/OAuthGetAccessToken',
 	        'consumer_key': 'anonymous',
-	        'consumer_secret':   'anonymous',
+	        'consumer_secret': 'anonymous',
 	        'scope': 'https://docs.google.com/feeds',
 	        'app_name': 'Readerplus sync settings'
 		}
