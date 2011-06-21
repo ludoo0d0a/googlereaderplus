bd45dbfb9e9e75c6935d31303c7ce8b845b894e0
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2011-06-21 14:50:17 +0000

    facebook / reddit popup

--- trunk/GoogleReaderPlus/src/facebook.js
+++ trunk/GoogleReaderPlus/src/facebook.js
@@ -14,58 +14,31 @@
  */
 
 GRP.facebook = function(prefs, langs, ID, SL, lang){
-	var BTN_CLS = 'item-share star';
-	var BTN_CLS_ID ='btn-'+ID+' '+BTN_CLS;
+	var BTN_CLS = 'item-share star', BTN_CLS_ID ='btn-'+ID+' '+BTN_CLS;
+	var URL='http://www.facebook.com/sharer.php?&u={url}&t={title}';
+	var OPTWIN = 'toolbar=0,status=0,resizable=1,width=626,height=436';
 	
 	function addButton(el, entry, mode) {
-		var title = SL.text + formatShortcut(ID, 'gofacebook', prefs); //[b]
+		var title = SL.text + formatShortcut(ID, 'go'+ID, prefs); //[b]
 		var text = (prefs && prefs.general_icons)?'':(SL.keyword || ID);
-		addBottomLink(el,text, title, ID, BTN_CLS, false, facebookShare, false, entry, mode);
+		addBottomLink(el,text, title, ID, BTN_CLS, false, openShareWindow, false, entry, mode);
 	}
 
 	function addKey() {
-		onKey('btn-facebook', facebookShare);
+		onKey('btn-'+ID, openShareWindow);
 	}
 
-	function facebookShare(btn, entry, locked) {
-		var active = isActive(btn, entry, 'facebook', locked, 'btn-active', 'btn-inactive');
+	function openShareWindow(btn, entry, locked) {
+		var active = isActive(btn, entry, ID, locked, 'btn-active', 'btn-inactive');
 		addClassIf(btn, 'item-star-active',active);
-		var iframe, facebookSharer;
-		var body = getFirstElementByClassName(entry,  'entry-body');//div
-		iframe = getFirstElementByClassName(entry,  'facebookSharer');//iframe
 		if (active) {
-			// iframe creation/display
 			var shareurl = getEntryLink(entry);
-			var e = encodeURIComponent;
-			var fbsharer = 'http://www.facebook.com/sharer.php?&u=' + e(shareurl.url) + '&t='
-					+ e(shareurl.title);
-			window.open(fbsharer, 'sharer', 'toolbar=0,status=0,resizable=1,width=626,height=436');
-			// Scale article container to fullwidth
-			body.setAttribute('style', gpeStyles.entryBody);
-		} else {
-			// hide iframe
-			iframe.style.display = 'none';
-
-			// Go back to initial width
-			body.removeAttribute('style', '');
-		}
-		scrollTo(150, active);
+			var url = fillTpl(URL, shareurl);
+			window.open(url, ID+'sharer', OPTWIN);
+		} 
 	}
-
-	function scrollTo(offset, dir) {
-		var view = document.getElementById('viewer-container');
-		view.scrollTop += ((dir) ? 1 : -1) * offset;
-		// Force scrolling to top of article
-		// location.href = 'javascript:void(s.V=-1);';
-		// location.href = 'javascript:void(s.pf('+index+'));';
-	}
-
-	var gpeStyles = {
-		entryBody : 'max-width: 98%'
-	};
-
 	registerFeature(addButton, ID);
-	var keycode = getShortcutKey(ID, 'gofacebook', prefs); //66 b
+	var keycode = getShortcutKey(ID, 'go'+ID, prefs); //66 b
 	keycode.fn = addKey;
 	initKey(keycode);
 };
