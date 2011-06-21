c36ed12842b3740cd4859d1350703f517bd18b6c
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2011-06-21 14:50:53 +0000

    mark as read as scroll

--- GoogleReaderPlus/src/mark.js
+++ GoogleReaderPlus/src/mark.js
@@ -11,7 +11,7 @@
  */
 
 GRP.mark = function(prefs, langs, ID, SL, lang){
-	var currentElement, modeList=false;
+	var currentElement, CLS='grp-mark-scrolled', CLS_READ='grp-mark-scroll-read', modeList=false, entries=get_id('entries');
 	function setConfig(){
 		currentElement = getCurrentEntry();
 		modeList=(getMode(currentElement)==='list');
@@ -48,4 +48,49 @@ GRP.mark = function(prefs, langs, ID, SL, lang){
     var keycodeDown = getShortcutKey('mark', 'marknext', prefs); //75;//k
     keycodeDown.fn = markAfterCurrentAsRead;
     initKey([keycodeUp, keycodeDown]);
+    
+    //Mark as reas as scroll
+    if (prefs[ID+'_asscroll']){
+		if(entries){
+			entries.addEventListener('scroll',checkScroll,false);
+		}
+	}
+	
+	function checkScroll(ev){
+		var mode = getMode();
+		if (mode!=='list'){
+			return;		
+		}
+		var all = entries.getElementsByClassName('entry');    
+		var els=[], st = entries.scrollTop , t=0, i=0;
+		var curr = getCurrentEntry();
+		while(t<st){
+			var entry=all[i++];
+			if (entry){
+				t+=entry.offsetHeight;
+				if (t<st){
+					if (!hasClass(entry,'read')){
+						els.push(entry);
+					}
+				}
+			}
+		}
+		if (els.length>0){
+			els=els.reverse();
+			for(var len = els.length, i = len-1; i>=0; i--){
+				var el = els[i];
+				if (!hasClass(el,CLS)){
+					addClass(el,CLS);
+					markasread(el, i==0);
+				}
+			}
+			entries.scrollTop=st;
+			setTimeout(function(){
+				if (curr){
+					selectCurrentEntry(curr);
+				}
+			},400);
+		}
+		
+	}
 };
