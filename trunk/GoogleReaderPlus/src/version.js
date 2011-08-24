3db791b14df8f5cb3b949bac935bfb325eb15511
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2011-08-24 09:04:07 +0000

    3.7.2 rank deprecated

--- trunk/GoogleReaderPlus/src/version.js
+++ trunk/GoogleReaderPlus/src/version.js
@@ -3,7 +3,7 @@
  */
 window.GRP={
 	VERSION: "3.7.2",
-	//beta:5,
+	//beta:1,
 	setVersion:function(text){
 		var ver = document.getElementById('version');
 		ver.innerHTML = (text||'') + GRP.VERSION;
