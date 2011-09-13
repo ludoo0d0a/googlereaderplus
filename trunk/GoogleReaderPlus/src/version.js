6fd847b578626a0bcefc30f1c340c8836a91583a
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2011-09-13 07:43:06 +0000

    v3.8.0

--- trunk/GoogleReaderPlus/src/version.js
+++ trunk/GoogleReaderPlus/src/version.js
@@ -3,7 +3,7 @@
  */
 window.GRP={
 	VERSION: "3.8.0",
-	beta:1,
+	//beta:1,
 	setVersion:function(text){
 		var ver = document.getElementById('version');
 		ver.innerHTML = (text||'') + GRP.VERSION;
