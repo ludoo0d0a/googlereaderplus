a659f75a4822e976d703b1487072145d597a0e5a
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2011-11-18 10:54:27 +0000

    skins prepared for new UI

--- trunk/GoogleReaderPlus/src/skin/helvetireader.js
+++ trunk/GoogleReaderPlus/src/skin/helvetireader.js
@@ -1,10 +1,10 @@
 /*
  * Helvetireader
  */
-GRP.helvetireader = function() {	
+GRP.helvetireader = function(prefs, langs, ID, SL, lang) {	
 	//helvetireader v2
 	var url= 'http://www.helvetireader.com/css/helvetireader.2.css';
-	GM_addCss(url, 'rps_helvetireader2');
+	GM_addCss(url, 'rps_'+ID);
 	
 	//custom changes for colorful listview conflict
 	var css = '#chrome-header{background-color:#33333!important;}';
@@ -12,7 +12,9 @@ GRP.helvetireader = function() {
 	css += '#lhn-add-subscription-section {height: 30px;}';
 	css += '#viewer-header {height:53px;}';
 	css += '#title-and-status-holder {display:none;}';
-	GM_addStyle(css, 'rpsfix_helvetireader2');
+	css += '#entries{padding-right:0px ;}';
+	
+	GM_addStyle(css, 'rpsfix_'+ID);
 	
 	fireResize();
 };
--- trunk/GoogleReaderPlus/src/skin/nativecompact.js
+++ trunk/GoogleReaderPlus/src/skin/nativecompact.js
@@ -1,11 +1,16 @@
 //nativecompact
-GRP.nativecompact = function() {
+GRP.nativecompact = function(prefs, langs, ID, SL, lang) {
 	var css = "#search{top:4px;left:600px;z-index:999;}#logo-container{display:none;}#main{top:0px;}#chrome-header,#viewer-top-controls{padding:0px;}#lhn-add-subscription-section{position: absolute;left:500px;top:-70px;z-index:999;}";
+	//hide search
 	css+='#top-bar{height:0;position:static !important;}';
+	//hide menu g+
+	css+='#gb{display:none;}';
 	var el = getFirstElementByClassName(get_id('lhn-add-subscription-section'), 'subscribe-button');
 	if (el){
 		el.innerText='';
 	}
-	GM_addStyle(css, 'rps_nativecompact');
+	css += '#entries{padding-right:0px ;}';
+	
+	GM_addStyle(css, 'rps_'+ID);
 	fireResize();
 };
