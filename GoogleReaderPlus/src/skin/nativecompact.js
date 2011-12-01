65453aebb84e321fb31424227c6b82d2665804c9
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2011-12-01 15:02:46 +0000

    fix nativecompact

--- GoogleReaderPlus/src/skin/nativecompact.js
+++ GoogleReaderPlus/src/skin/nativecompact.js
@@ -1,10 +1,12 @@
 //nativecompact
 GRP.nativecompact = function(prefs, langs, ID, SL, lang) {
-	var css = "#search{top:4px;left:600px;z-index:999;}#logo-container{display:none;}#main{top:0px;}#chrome-header,#viewer-top-controls{padding:0px;}#lhn-add-subscription-section{position: absolute;left:500px;top:-70px;z-index:999;}";
+	var css = '#search{top:4px;left:600px;z-index:999;}#logo-container{display:none;}#main{top:0px;}#chrome-header,#viewer-top-controls{padding:0px;}';
+	css += '#quick-add-bubble-holder{margin-top: 30px;}';
+	
 	//hide search
 	css+='#top-bar{height:0;position:static !important;}';
 	//hide menu g+
-	css+='#gb{display:none;}';
+	css+='#gb,#lhn-add-subscription-section,#viewer-header,#logo-section,#title-and-status-holder{display:none;}';
 	var el = getFirstElementByClassName(get_id('lhn-add-subscription-section'), 'subscribe-button');
 	if (el){
 		el.innerText='';
