37a832dec15582ddf477a169b9bd460aa019b70d
ludovic.valente@gmail.com <ludovic.valente@gmail.com@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2011-12-07 10:22:12 +0000

    Fixed+tests

--- trunk/GoogleReaderPlus/src/lib/date.js
+++ trunk/GoogleReaderPlus/src/lib/date.js
@@ -15,25 +15,37 @@ var dDate = function(){
 			},
 			full:{
 				//Nov 16, 2011 6:10 PM
-				re:/((\w+)\s(\d+),\s(\d+))\s?((\d+):(\d+)\s([AP]M))(\s+\([^\)]\))/,
-				p:['_day','M','d','y','_hour','h','m','t','_ago','ago']
+				re:/((\w+)\s(\d+),\s(\d+))?\s?((\d+):(\d+)\s([AP]M))?(\s+\([^\)]\))?/,
+				p:['_all','_day','M','d','y','_hour','h','m','t','_ago','ago']
 			},
 			ago:/\d+ hours ago/
 		}
 	};
 	
-	return function(){
+	function getInt(a,def){
+    var v = a;
+    if (v){
+      v=parseInt(v,10);
+    }
+    return v ||def;
+	}
+	
+	//return function(){
 		return {
 			parse:function(text, lang){
 				var d = false;
-				var l = langs[lang];
-				var m = l.full.re.exec(text);
+				lang=lang||'en';
+				var l = langs[lang],m=l.full.re.exec(text),p=l.full.p;
 				if (m){
 					var n={y:0, M:0, d:0, h:0, m:0, s:0};
 					//d = new Date(year, month, day, hours, minutes, seconds, milliseconds);
 					for(var i=0, len=m.length;i<len; i++){
-						n[p[i]]=m[i];
+						n[p[i]]=m[i]||0;
 					}
+					var today = new Date();
+					n.y=getInt(n.y,today.getFullYear());
+					n.M=getInt(n.M,today.getMonth());
+					n.d=getInt(n.d,today.getDate());
 					d = new Date(n.y, n.M, n.d, n.h, n.m, n.s, 0);
 				}
 				return d;
@@ -46,14 +58,32 @@ var dDate = function(){
 				} else{
 					return -1;
 				}
+			},
+			daysBetween:function(d1,d2){
+        var ms = d1.getTime()-d2.getTime();
+        return Math.floor(ms/864E5);
 			}
 		};
-	};
+	//};
 	
 }();
 
+function test(txt){
+  var today = new Date();
+  var d = dDate.parse(txt);
+  var days = dDate.daysBetween(d,today);
+  console.log(txt+' -> '+d + 'days='+days);
+}
 
-// For convenience...
-Date.prototype.compare = function (date) {
-	return dDate.compare(this, date);
-};
+function tests(){
+  test('10:38 AM');
+  test('Dec 6, 2011');
+  test('Nov 13, 2011');
+  test('Sep 29, 2011');
+  
+  test('Dec 5, 2011 (2 days ago)');
+  test('Dec 1, 2011 (6 days ago)');
+  test('Nov 26, 2011 5:42 PM');
+  test('Nov 22, 2011 10:09 AM');
+}
+//tests();
