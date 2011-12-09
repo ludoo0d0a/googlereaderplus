8726e06d02c40b1feeaa626530a74cdb36e43d7b
ludovic.valente@gmail.com <ludovic.valente@gmail.com@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2011-12-09 10:20:38 +0000

    rollback to dynamic version

--- trunk/GoogleReaderPlus/src/prefetch.js
+++ trunk/GoogleReaderPlus/src/prefetch.js
@@ -12,10 +12,10 @@
  */
 GRP.prefetch = function(prefs, langs, ID, SL, lang){
 	var first=prefs.prefetch_first||25, next =prefs.prefetch_next||15, list=prefs.prefetch_list||60;
-	//var js = 'var GRP_prefetch={first:'+first+','+'next:'+next+','+	'list:'+list+'};';
-	//GM_addjs(js, true, '_grp_prefetch');
-	//GM_addjs(chrome.extension.getURL('res/prefetch.js?'+Math.round(Math.random()*999+1)));
-	
+	var js = 'var GRP_prefetch={first:'+first+','+'next:'+next+','+	'list:'+list+'};';
+	GM_addjs(js, true, '_grp_prefetch');
+	GM_addjs(chrome.extension.getURL('res/prefetch.js?'+Math.round(Math.random()*999+1)));
+	/*
 	function injectJs(js){
 		//mycore.tabs.executeScript(null,{code:js});
 		GM_addjs(js, true, '_grp_prefetch');
@@ -24,5 +24,5 @@ GRP.prefetch = function(prefs, langs, ID, SL, lang){
     var script = 'cv.prototype.mo=function (a,b){this.$=Hq.ia(a,'+next+');b()};';
     script+='cv.prototype.Ql=function (){return Yu(this.ub())?this.Jb?'+first+':'+next+':'+list+'};';
 	injectJs(script);
-
+*/
 };
