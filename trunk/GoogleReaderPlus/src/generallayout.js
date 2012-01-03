138ea88ca1c1170cd764aa9647f05bc3a609d9c6
ludovic.valente@gmail.com <ludovic.valente@gmail.com@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2012-01-03 08:59:27 +0000

    focus current

--- trunk/GoogleReaderPlus/src/generallayout.js
+++ trunk/GoogleReaderPlus/src/generallayout.js
@@ -254,10 +254,14 @@ GRP.generallayout = function(prefs, langs, ID, SL, lang) {
 		//star only, strange way todo...
 		css += '#current-entry .entry-actions > span.star,#entries.list #current-entry .entry-actions > span.star{width:0px}';
 		css += '#current-entry .entry-main{margin-right:'+w+'px}';//Current reduced for current floating menu
-css += '.card-common .card-actions{height:auto !important}';
-//css += '#entries .entry:not(#current-entry){opacity:0.9 !important}';
+		css += '.card-common .card-actions{height:auto !important}';
 
 		GM_addStyle(css, 'rpe_floatactions');
+	}
+	
+	if (getPref('focuscurrent')){
+		var css = '#entries .entry:not(#current-entry){opacity:0.6 !important}';
+		GM_addStyle(css, 'rpe_focuscurrent');
 	}
 
 	if (getPref('floatactions')) {
