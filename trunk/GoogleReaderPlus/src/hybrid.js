ade706ecd7ade9dd48541a34f67ab9734dd7871e
ludovic.valente@gmail.com <ludovic.valente@gmail.com@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2012-01-09 15:31:01 +0000

    +hybrid

---
+++ trunk/GoogleReaderPlus/src/hybrid.js
@@ -0,0 +1,44 @@
+/**
+ *  Mode hybrid = mode beween card and list
+ *  list with bigger row + picture 
+ */
+GRP.hybrid = function(prefs, langs, ID, SL, lang){
+	var btnMode, mycss='', css = '.entry:not(#current-entry) .card{max-height:120px;}';
+    css += '.entry-main{padding-left:140px;min-height:60px;}';
+    css += '.entry-body img.entry-icon-preview{position:absolute;top:0;left:0px;max-width:130px;max-height:130px}';
+    css += '.card-actions{display:none;}';
+    css += '#current-entry .card-actions{display:block;}';
+    
+	function hybridEntry(el, entry, mode){
+		var img = findImage({el:entry}, function(img){
+			if (img){
+				addClass(img, 'entry-icon-preview');
+			}
+		});
+	}
+	
+	function addModeButton(){
+		btnMode = dh('stream-view-options-container', 'div', {
+			position:'first',
+			role:'button', 
+			cls:'goog-inline-block jfk-button jfk-button-standard jfk-button-narrow jfk-button-collapse-right jfk-button-clear-outline',
+			style:'-webkit-user-select: none; ',
+			title:'Hybrid mode',
+			html: '<img src="/reader/ui/138769644-magnifying_glass.png" style="width: 21px; height: 21px; " class="jfk-button-img">'
+		},{click:function(e){
+			toggleTheme();
+		}});
+	}
+	
+    function toggleTheme(){
+    	mycss= (mycss)?'':css;
+    	GM_addStyle(mycss, 'rps_'+ID);
+    	addClass(btnMode, 'jfk-button-checked', mycss);
+    }
+    
+    addModeButton();
+    
+	registerFeature(hybridEntry, ID, {
+    	onlistviewtitle:false
+    });
+};
