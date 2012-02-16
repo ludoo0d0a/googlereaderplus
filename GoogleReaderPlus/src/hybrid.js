76f0f62389b50467ea6c3e1c8b1eab1d675dedbe
ludovic.valente <ludovic.valente@ad7a2c28-ecb1-11de-9b30-9f9d59cce923>
2012-02-16 08:35:17 +0000

    icon

--- GoogleReaderPlus/src/hybrid.js
+++ GoogleReaderPlus/src/hybrid.js
@@ -1,9 +1,10 @@
 /**
- *  Mode hybrid = mode beween card and list
+ *  Mode hybrid = mode between card and list
  *  list with bigger row + picture 
  */
 GRP.hybrid = function(prefs, langs, ID, SL, lang){
 	var btnMode, mycss='';
+	var ICONS_PATH = 'http://googlereaderplus.googlecode.com/svn/trunk/GoogleReaderPlus/images/toolbar/black/';
 	var css = '.entry:not(#current-entry) .card{max-height:120px;}';
     css += '.entry-main{padding-left:140px;min-height:60px;}';
     css += '.entry-body img.entry-icon-preview{position:absolute;top:0;left:0px;max-width:130px;max-height:130px}';
@@ -26,7 +27,7 @@ GRP.hybrid = function(prefs, langs, ID, SL, lang){
 			cls:'goog-inline-block jfk-button jfk-button-standard jfk-button-narrow jfk-button-collapse-right jfk-button-clear-outline',
 			style:'-webkit-user-select: none; ',
 			title:'Hybrid mode',
-			html: '<img src="/reader/ui/138769644-magnifying_glass.png" style="width: 21px; height: 21px; " class="jfk-button-img">'
+			html: '<img src="'+ICONS_PATH+'hybrid.png" style="width: 21px; height: 21px; " class="jfk-button-img">'
 		},{click:function(e){
 			toggleTheme();
 		}});
@@ -35,7 +36,7 @@ GRP.hybrid = function(prefs, langs, ID, SL, lang){
     function toggleTheme(){
     	mycss= (mycss)?'':css;
     	GM_addStyle(mycss, 'rps_'+ID);
-    	addClass(btnMode, 'jfk-button-checked', mycss);
+    	addClassIf(btnMode, 'jfk-button-checked', mycss);
     }
     
     render();
